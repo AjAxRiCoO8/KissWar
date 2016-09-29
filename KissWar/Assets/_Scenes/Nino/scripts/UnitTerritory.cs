c2ae919066a3bae4af6a2170129cfb7616d3b057
Nino Thijs <ninothijs@gmail.com>
2016-09-29 12:06:52 +0200

    Scripts for unit and enemy
    ^

---
+++ KissWar/Assets/_Scenes/Nino/scripts/UnitTerritory.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitTerritory : MonoBehaviour {
+
+   /* public BoxCollider2D territory;
+    GameObject enemy;
+    GameObject unit;
+
+    bool enemyInTerritory;
+
+    FightControl fightControl;
+
+    // Use this for initialization
+    void Start () {
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+        unit = GameObject.FindGameObjectWithTag("Unit");
+        fightControl = fightControl.GetComponent<FightControl>();
+        enemyInTerritory = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(enemyInTerritory == true)
+        {
+            fightControl.MoveUnit();
+        }
+
+      
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            enemyInTerritory = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            enemyInTerritory = false;
+        }
+    }
+    */
+}
